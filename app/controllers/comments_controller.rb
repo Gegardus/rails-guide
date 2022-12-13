d64fbd8eae89100c8f38bd076d5b06cd53dbfb96
Gegardus <stgegard@gmail.com>
2022-12-13 14:06:13 +0400

    add security

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,23 @@
 class CommentsController < ApplicationController
-    def create 
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.create(comment_params)
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
-      redirect_to article_path(@article)
-    end
+  def create 
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
 
-    def destroy 
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.find(params[:id])
-      @comment.destroy
+    redirect_to article_path(@article)
+  end
 
-      redirect_to article_path(@article), status: :see_other 
-    end
+  def destroy 
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
 
-    private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body, :status) 
-    end
+    redirect_to article_path(@article), status: :see_other 
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status) 
+  end
 end
